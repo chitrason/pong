7a5371eba23a0851477a4607553647ee223f4ca6
Mcswiggi <mrchitrason@gmail.com>
2022-12-12 17:00:06 -0500

    add scoreboard functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """Pong"""
 from turtle import Screen, Turtle
 from paddle import Paddle
-
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,8 +14,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-
-
+ball = Ball()
+scoreboard = Scoreboard()
 
 
 
@@ -26,9 +28,25 @@ screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
+  time.sleep(.1)
   screen.update()
+  ball.move()
 
+  # detect collision with wall
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    # needs to bounce
+    ball.bounce_y()
 
+  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    ball.bounce_x()
+
+  if ball.xcor() > 380:
+    ball.reset_postition()
+    scoreboard.l_point()
+  
+  if ball.xcor() < -380:
+    ball.reset_postition()
+    scoreboard.r_point()
 
 
 
